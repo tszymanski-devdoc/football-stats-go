006ebdb25b0bad0c5773f49238e850f6feb5a6e9
Tomasz Szymanski <tszymanski.devdoc@gmail.com>
2025-06-11 15:03:05 +0200

    actions changes and ui added

--- football-stats-go-api/main.go
+++ football-stats-go-api/main.go
@@ -13,7 +13,17 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 }
 
+func statsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Football stats data"))
+		return
+	}
+	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+}
+
 func main() {
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/stats", statsHandler)
 	http.ListenAndServe(":8080", nil)
 }
